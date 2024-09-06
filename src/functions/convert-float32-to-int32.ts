281a9dc36f0e50a2f12cd65a393995e4f3fff21a
Christoph Guttandin <chrisguttandin@media-codings.com>
2024-09-07 01:58:08 +0200

    fix: prevent integer overflow

--- src/functions/convert-float32-to-int32.ts
+++ src/functions/convert-float32-to-int32.ts
@@ -1,1 +1,1 @@
-export const convertFloat32ToInt32 = (value: number) => Math.fround(value * (value < 0 ? 2147483648 : 2147483647));
+export const convertFloat32ToInt32 = (value: number) => Math.min(Math.fround(value * (value < 0 ? 2147483648 : 2147483647)), 2147483647);
--- src/functions/convert-int16-to-int32.ts
+++ src/functions/convert-int16-to-int32.ts
@@ -1,1 +1,1 @@
-export const convertInt16ToInt32 = (value: number) => Math.fround(value * (value < 0 ? 65536 : 65538));
+export const convertInt16ToInt32 = (value: number) => Math.min(Math.fround(value * (value < 0 ? 65536 : 65538)), 2147483647);
