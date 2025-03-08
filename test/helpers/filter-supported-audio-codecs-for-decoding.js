9b2400693c1703abb70ee14ead5604dbc89bcff2
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-03-08 13:52:09 +0100

    test: update supported codecs for decoding for Chrome v134

--- test/helpers/filter-supported-audio-codecs-for-decoding.js
+++ test/helpers/filter-supported-audio-codecs-for-decoding.js
@@ -1,5 +1,5 @@
 export const filterSupportedAudioCodecsForDecoding = (knownAudioCodecs, userAgent) =>
-    /Chrome\/13[45]/.test(userAgent)
+    /Chrome/.test(userAgent)
         ? knownAudioCodecs.filter((knownAudioCodec) =>
               [
                   'alaw',
@@ -23,7 +23,7 @@ export const filterSupportedAudioCodecsForDecoding = (knownAudioCodecs, userAgen
                   'vorbis'
               ].includes(knownAudioCodec)
           )
-        : /Chrome/.test(userAgent)
+        : /Firefox/.test(userAgent)
           ? knownAudioCodecs.filter((knownAudioCodec) =>
                 [
                     'alaw',
@@ -35,33 +35,14 @@ export const filterSupportedAudioCodecsForDecoding = (knownAudioCodecs, userAgen
                     'mp4a.40.29',
                     'mp4a.40.5',
                     'mp4a.67',
-                    'mp4a.69',
-                    'mp4a.6B',
                     'opus',
+                    'pcm-f32',
+                    'pcm-s16',
+                    'pcm-s24',
+                    'pcm-s32',
+                    'pcm-u8',
                     'ulaw',
                     'vorbis'
                 ].includes(knownAudioCodec)
             )
-          : /Firefox/.test(userAgent)
-            ? knownAudioCodecs.filter((knownAudioCodec) =>
-                  [
-                      'alaw',
-                      'flac',
-                      'mp3',
-                      'mp4a.40.02',
-                      'mp4a.40.05',
-                      'mp4a.40.2',
-                      'mp4a.40.29',
-                      'mp4a.40.5',
-                      'mp4a.67',
-                      'opus',
-                      'pcm-f32',
-                      'pcm-s16',
-                      'pcm-s24',
-                      'pcm-s32',
-                      'pcm-u8',
-                      'ulaw',
-                      'vorbis'
-                  ].includes(knownAudioCodec)
-              )
-            : [];
+          : [];
