26fc3078704524b41bb855366da4201e6f6ddb20
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-10-14 00:00:35 +0200

    test: update supported codecs for Safari v26

--- test/helpers/filter-supported-audio-codecs-for-decoding.js
+++ test/helpers/filter-supported-audio-codecs-for-decoding.js
@@ -45,4 +45,23 @@ export const filterSupportedAudioCodecsForDecoding = (knownAudioCodecs, userAgen
                     'vorbis'
                 ].includes(knownAudioCodec)
             )
-          : [];
+          : knownAudioCodecs.filter((knownAudioCodec) =>
+                [
+                    'alaw',
+                    'flac',
+                    'mp3',
+                    'mp4a.40.02',
+                    'mp4a.40.05',
+                    'mp4a.40.2',
+                    'mp4a.40.29',
+                    'mp4a.40.5',
+                    'opus',
+                    'pcm-f32',
+                    'pcm-s16',
+                    'pcm-s24',
+                    'pcm-s32',
+                    'pcm-u8',
+                    'ulaw',
+                    'vorbis'
+                ].includes(knownAudioCodec)
+            );
--- test/helpers/filter-supported-audio-codecs-for-encoding.js
+++ test/helpers/filter-supported-audio-codecs-for-encoding.js
@@ -5,4 +5,20 @@ export const filterSupportedAudioCodecsForEncoding = (knownAudioCodecs, userAgen
           )
         : /Firefox/.test(userAgent)
           ? knownAudioCodecs.filter((knownAudioCodec) => ['opus', 'vorbis'].includes(knownAudioCodec))
-          : [];
+          : knownAudioCodecs.filter((knownAudioCodec) =>
+                [
+                    'alaw',
+                    'mp4a.40.02',
+                    'mp4a.40.05',
+                    'mp4a.40.2',
+                    'mp4a.40.29',
+                    'mp4a.40.5',
+                    'opus',
+                    'pcm-f32',
+                    'pcm-s16',
+                    'pcm-s24',
+                    'pcm-s32',
+                    'pcm-u8',
+                    'ulaw'
+                ].includes(knownAudioCodec)
+            );
